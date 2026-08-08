6f5fc9abb96928e3d85bb20b77de3e1a267d9cef
Mahsa Teimourikia <mahsa.teimourikia@gmail.com>
2026-08-08 15:59:09 -0700

    Add adaptive RAG routing foundations

---
+++ src/adaptive_rag/__init__.py
@@ -0,0 +1,5 @@
+"""Deterministic Adaptive RAG routing examples for the learning track."""
+
+from .router import RouteDecision, adaptive_answer, classify_query, choose_k
+
+__all__ = ["RouteDecision", "adaptive_answer", "classify_query", "choose_k"]
